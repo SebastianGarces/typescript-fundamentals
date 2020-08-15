d3e91eb1883e219d6d9701c80d8ef0b123e77463
Sebastian Garces <gsgarces1@gmail.com>
2020-08-15 11:12:38 -0400

    completed course

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,36 @@
-export type Dict = {};
+// A dictoniary is an array where all of its values are of the same type.
+export type Dict<T> = {
+	[k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(dict: Dict<T>, fn: (arg: T, idx: number) => S): Dict<S> {
+	const out: Dict<S> = {};
+	Object.keys(dict).forEach((dKey, idx) => {
+		const thisItem = dict[dKey];
+		if (typeof thisItem !== "undefined") {
+			out[dKey] = fn(thisItem, idx);
+		}
+	});
+	return out;
+}
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T, R>(
+	dict: Dict<T>,
+	reducer: (acc: R, item: T, idx: number) => R,
+	initialValue: R
+): R {
+	// start with 0
+	let currentSum: R = initialValue;
+	// iterate
+	Object.keys(dict).forEach((key, idx) => {
+		// get current value of item
+		const val = dict[key];
+		if (typeof val !== "undefined") {
+			// add item to the "running total"
+			currentSum = reducer(currentSum, val, idx);
+		}
+	});
+	return currentSum; // return the final total
+}
